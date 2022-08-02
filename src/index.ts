d69471cf9a1fb970d3016fa8644c6b684fcb5f81
Marwan Mostafa <74471326+Marwan-Dev@users.noreply.github.com>
2022-08-03 01:37:48 +0200

    configure server and jasmine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello World Marwan');
+import express, { Application, Request, Response } from 'express';
+
+const PORT = 3000;
+// create server instance
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'hello World',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
+
+export default app;
