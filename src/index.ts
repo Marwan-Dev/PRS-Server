1b8d549bbd60417bf9f9f643b88cfff10de3ba0f
Marwan Mostafa <74471326+Marwan-Dev@users.noreply.github.com>
2022-08-03 16:00:23 +0200

    add middlewares logger and security

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const PORT = 3000;
 // create server instance
 const app: Application = express();
+// middleware to parse incoming requests
+app.use(express.json());
 
+// HTTP request logger middleware
+app.use(morgan('common'));
+
+// HTTP Security middleware
+app.use(helmet());
+
+// Apply the rate limiting middleware to all requests
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'Too many requests, please try again after 15 minutes',
+  })
+);
+
+// add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'hello World',
   });
 });
 
+// post request
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: 'hello World from post request',
+    data: req.body,
+  });
+});
+
+// listen to port
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 });
